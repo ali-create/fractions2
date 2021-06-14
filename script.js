d804848da6c842df13b89266f64bd13db001fdb4
ali-create <57019728+ali-create@users.noreply.github.com>
2021-06-14 18:29:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-let fractionOneN;
-let fractionOneD;
+let inputZero;
+let inputOne;
 let f1r;
 let f2r;
-let fractionTwoN;
-let fractionTwoD;
+let inputTwo;
+let inputThree;
 let f1;
 let f2;
 let plainText = document.querySelector(".plainText");
 document.querySelector(".btn").addEventListener("click", function () {
-  fractionOneN = document.querySelector(".input--0").value;
-  fractionOneD = document.querySelector(".input--1").value;
-  fractionTwoN = document.querySelector(".input--2").value;
-  fractionTwoD = document.querySelector(".input--3").value;
+  inputZero = document.querySelector(".input--0").value;
+  inputOne = document.querySelector(".input--1").value;
+  inputTwo = document.querySelector(".input--2").value;
+  inputThree = document.querySelector(".input--3").value;
 
-  f1r = `${fractionOneN}/${fractionTwoN}`;
-  f2r = `${fractionOneD}/${fractionTwoD}`;
+  f1r = `${inputZero}/${inputTwo}`;
+  f2r = `${inputOne}/${inputThree}`;
 
-  f1 = (100 / fractionOneN) * fractionTwoD;
+  f1 = (100 / inputZero) * inputTwo;
 
-  f2 = (100 / fractionTwoN) * fractionOneD;
-
-  if (f1 < f2) {
+  f2 = (100 / inputOne) * inputThree;
+  if (f2 < f1) {
     plainText.textContent = `${f2r} is larger than ${f1r}.`;
-  } else if (f2 > f1) {
+  } else if (f1 < f2) {
     plainText.textContent = `${f1r} is larger than ${f2r}.`;
   } else if ((f2 = f1)) {
     plainText.textContent = `${f2r} is equal to ${f1r}.`;
